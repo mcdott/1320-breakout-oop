2258b6f6a979e417894c570b46d57a98a31f6c08
mcdott <111994803+mcdott@users.noreply.github.com>
2023-01-26 13:18:34 -0500

    Updated the Ball class to extend Sprite

--- Ball.js
+++ Ball.js
@@ -1,21 +1,18 @@
-class Ball {
-  constructor(x = 0, y = 0, dx = 2, dy = -2, radius = 10, color = 'red') {
-    this.x = x;
-    this.y = y;
+class Ball extends Sprite {
+  constructor(x = 0, y = 0, dx = 2, dy = -1, radius = 10, color) {
+    super(x, y, radius * 2, radius * 2, color); // radius * 2 is the circle "width" and "height"
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
-    this.color = color;
     this.PI2 = Math.PI * 2;
   }
 
   // Moves the ball by updating positional x and y values
   move() {
-    this.x += this.dx;
-    this.y += this.dy;
+    this.moveBy(this.dx, this.dy);
   }
 
-  // Draws the ball on the screen
+  // Draws the ball on the screen (overides render method in Sprite)
   render(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, this.PI2);
